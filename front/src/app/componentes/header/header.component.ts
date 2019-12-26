41a5be47a80906aed981e1e923fa4c4faa87d8df
vgallardog <vgallardog@tgr.cl>
2019-12-26 18:06:22 +0000

    dejando cognito pre-lsito

--- front/src/app/componentes/header/header.component.ts
+++ front/src/app/componentes/header/header.component.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {UserService} from '../../servicios/user.service';
+import {CookieService} from 'ngx-cookie-service';
+import {CognitoService} from '../../servicios/cognito.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  estadoBotonLoggin;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userservice: UserService, private cognito: CognitoService, private cookieService: CookieService) { }
 
   ngOnInit() {
+    
+    if(!this.userservice.isLogged()){
+      this.estadoBotonLoggin = false;
+    }
+    
+    if(this.userservice.isLogged()){
+      this.estadoBotonLoggin = true;
+    }
   }
   
   iniciarSesion(){
+    
      this.router.navigate(['autentica']);
   }
+  
+  
+  cerrarSesion(){
+    this.cookieService.deleteAll('/');
+    this.cognito.logout();
+  }
+
 
 }
