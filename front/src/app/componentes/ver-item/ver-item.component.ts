92985c26142d9b7b4a337ccfa98bcac3b7a97529
ssilvaa <ssilvaa@tgr.cl>
2020-01-03 17:53:51 +0000

    cambios finales 03-01-2020

--- front/src/app/componentes/ver-item/ver-item.component.ts
+++ front/src/app/componentes/ver-item/ver-item.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'APELLIDO PATERNO', weight: 'Orellana'},
+  {position: 2, name: 'APELLIDO MATERNO', weight: 'Espinoza'},
+  {position: 3, name: 'NUMERO DE RUT', weight: '12.465.349-5'},
+  {position: 5, name: 'NOMBRES', weight: 'Tatiana Elizabeth'},
+  {position: 6, name: 'DIRECCIÓN', weight: 'Bio Bio 1485, 54 E, Edificio Bio Bio'},
+  {position: 7, name: 'ORDEN O FOLIO', weight: '17209502'},
+  {position: 8, name: 'COMUNA', weight: 'Santiago'},
+  {position: 10, name: 'NUMERO DE CUOTA', weight: '2'},
+  {position: 15, name: 'FECHA DE VENCIMIENTO', weight: '31/03/2011'},
+  {position: 91, name: 'TOTAL A PAGAR PLAZO', weight: '$17.039'},
+];
+
 @Component({
   selector: 'app-ver-item',
   templateUrl: './ver-item.component.html',
   styleUrls: ['./ver-item.component.scss']
 })
 export class VerItemComponent implements OnInit {
+  
+  displayedColumns: string[] = ['position', 'name', 'weight'];
+  dataSource = ELEMENT_DATA;
 
   constructor(private cdRef:ChangeDetectorRef) { }
  @Input() movConsultado:{}
